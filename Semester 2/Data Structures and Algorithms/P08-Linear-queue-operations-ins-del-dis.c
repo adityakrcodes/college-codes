7590f5811faf6188c4506370bec84464726feee4
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2023-04-09 16:40:36 +0530

    added indentation

--- Semester 2/Data Structures and Algorithms/P08-Linear-queue-operations-ins-del-dis.c
+++ Semester 2/Data Structures and Algorithms/P08-Linear-queue-operations-ins-del-dis.c
@@ -54,19 +54,19 @@ void main(){
     int ch;
     clrscr();
     while (1){
-    printf("\n\t\t\tQUEUE OPERATIONS Program \n");
-    printf("\n\t\t\tEnter your choice: \n");
-    printf("\n\t\t\t1. Insert \n");
-    printf("\n\t\t\t2. Delete \n");
-    printf("\n\t\t\t3. Display \n");
-    printf("\n\t\t\t4. Exit \n");
-    scanf("%d",&ch);
-    switch (ch){
-        case 1: Insert();break;
-        case 2: Delete();break;
-        case 3: Display();break;
-        case 4: exit(0);
-        default: printf("Invalid choice");
-        }
+        printf("\n\t\t\tQUEUE OPERATIONS Program \n");
+        printf("\n\t\t\tEnter your choice: \n");
+        printf("\n\t\t\t1. Insert \n");
+        printf("\n\t\t\t2. Delete \n");
+        printf("\n\t\t\t3. Display \n");
+        printf("\n\t\t\t4. Exit \n");
+        scanf("%d",&ch);
+        switch (ch){
+            case 1: Insert();break;
+            case 2: Delete();break;
+            case 3: Display();break;
+            case 4: exit(0);
+            default: printf("Invalid choice");
+            }
     }
 }
