5586d3bd622b84f01c2d89a1b7575b0186c7e0c7
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2023-05-18 02:54:55 +0530

    Formatted the code

--- Semester 2/Object-Oriented Programming using JAVA/student_info_package_prgm.java
+++ Semester 2/Object-Oriented Programming using JAVA/student_info_package_prgm.java
@@ -1,10 +1,10 @@
 import BCA.*;
+
 public class student_info_package_prgm {
-    
-    public static void main(String args[])
-    {
-    bca_student_package obj = new bca_student_package();
-    obj.accept();
-    obj.display();
-}
+
+    public static void main(String args[]) {
+        bca_student_package obj = new bca_student_package();
+        obj.accept();
+        obj.display();
+    }
 }
