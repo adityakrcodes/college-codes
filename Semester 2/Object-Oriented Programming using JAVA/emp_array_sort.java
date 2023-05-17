cef50e0599cb934fa19eeb4c150b182601f99eb9
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2023-05-18 01:12:48 +0530

    Modified according to the question.

--- Semester 2/Object-Oriented Programming using JAVA/emp_array_sort.java
+++ Semester 2/Object-Oriented Programming using JAVA/emp_array_sort.java
@@ -2,14 +2,21 @@ import java.util.Arrays;
 
 public class emp_array_sort {
     public static void main(String[] args){
-        Employee[] staff = new Employee[3];
+        Employee[] staff = new Employee[10];
         staff[0] = new Employee("Krishna", 40000);
         staff[1] = new Employee("Ram", 60000);
         staff[2] = new Employee("Arjun", 50000);
+        staff[3] = new Employee("Bheem", 70000);
+        staff[4] = new Employee("Nakul", 30000);
+        staff[5] = new Employee("Sahadev", 20000);
+        staff[6] = new Employee("Duryodhan", 10000);
+        staff[7] = new Employee("Dushasan", 80000);
+        staff[8] = new Employee("Karna", 90000);
+        staff[9] = new Employee("Shakuni", 100000);
 
         Arrays.sort(staff); // Sort the staff array
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 10; i++)
             staff[i].print();
     }
 }
