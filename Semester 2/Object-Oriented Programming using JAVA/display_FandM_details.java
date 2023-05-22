38ef34162f5c7233910fb12e89525120e5fac215
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2023-05-22 17:48:59 +0530

    Fixed Indentation and the info shown was swapped

--- Semester 2/Object-Oriented Programming using JAVA/display_FandM_details.java
+++ Semester 2/Object-Oriented Programming using JAVA/display_FandM_details.java
@@ -1,61 +1,61 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.event.WindowListener;
-import java.awt.event.WindowEvent;
 
 public class display_FandM_details extends WindowAdapter {
 
-    display_FandM_details(){
-        Frame frame=new Frame("Details");
-        Label label=new Label();
-        Label label1=new Label();
-        Label label2=new Label();
-        Label label3=new Label();
-        Button button=new Button("Mother");
-        button.setBounds(200,60,60,60);
-        Button button1=new Button("Father");
-        button1.setBounds(300,60,60,60);
-        button.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                label.setBounds(100,250,100,20);
-                label1.setBounds(100,270,100,20);
-                label2.setBounds(100,290,110,20);
-                label3.setBounds(100,310,200,20);
+    display_FandM_details() {
+        Frame frame = new Frame("Details");
+        Label label = new Label();
+        Label label1 = new Label();
+        Label label2 = new Label();
+        Label label3 = new Label();
+        Button button = new Button("Mother");
+        button.setBounds(200, 60, 60, 60);
+        Button button1 = new Button("Father");
+        button1.setBounds(300, 60, 60, 60);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                label.setBounds(100, 250, 100, 20);
+                label1.setBounds(100, 270, 100, 20);
+                label2.setBounds(100, 290, 110, 20);
+                label3.setBounds(100, 310, 200, 20);
+                label.setText("Name:RADHA");
+                label1.setText("Age:45");
+                label2.setText("Designation: CEO");
+                label3.setText("Phone:9711770164");
+            }
+        });
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                label.setBounds(100, 250, 100, 20);
+                label1.setBounds(100, 270, 100, 20);
+                label2.setBounds(100, 290, 150, 20);
+                label3.setBounds(100, 310, 200, 20);
                 label.setText("Name:KRISHNA");
                 label1.setText("Age: 50");
                 label2.setText("Designation: Manager");
                 label3.setText("Phone:9711770164");
             }
         });
-    button1.addActionListener(new ActionListener(){
-        public void actionPerformed(ActionEvent e){
-            label.setBounds(100,250,100,20);
-            label1.setBounds(100,270,100,20);
-            label2.setBounds(100,290,110,20);
-            label3.setBounds(100,310,200,20);
-            label.setText("Name:RADHA");
-            label1.setText("Age:45");
-            label2.setText("Designation: CEO");
-            label3.setText("Phone:9711770164");
-        }
-    });
-    frame.addWindowListener(new WindowAdapter(){
-        public void windowClosing(WindowEvent evt){
-            frame.dispose();
-        }
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent evt) {
+                frame.dispose();
+            }
 
-    });
-    frame.add(button);
-    frame.add(button1);
-    frame.add(label);
-    frame.add(label1);
-    frame.add(label2);
-    frame.add(label3);
-    frame.setSize(500,500);
-    frame.setVisible(true);
-    frame.setLayout(null);
+        });
+        frame.add(button);
+        frame.add(button1);
+        frame.add(label);
+        frame.add(label1);
+        frame.add(label2);
+        frame.add(label3);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setLayout(null);
     }
-public static void main(String[]args){
-    display_FandM_details d =new display_FandM_details();
+
+    public static void main(String[] args) {
+        display_FandM_details d = new display_FandM_details();
     }
 }
