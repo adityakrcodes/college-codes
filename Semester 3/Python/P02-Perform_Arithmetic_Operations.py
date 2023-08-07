c7397cd09883fc976fd0590222a32f002d35e655
Abdul Bayees <bayees1@gmail.com>
2023-08-07 17:51:07 +0530

    added program2

---
+++ Semester 3/Python/P02-Perform_Arithmetic_Operations.py
@@ -0,0 +1,9 @@
+# Write a python program to perform diffrent arithmetic operations on numbers in python.
+
+a=int(input("Enter the value of var a:"))
+b=int(input("Enter the value of var b:"))
+print("Addition:",(a+b))
+print("Substraction:",(a-b))
+print("Multiplication:",(a*b))
+print("Division:",(a/b))
+print("Addition of {0} and {1} is: {2}".format(a,b,(a+b)))
